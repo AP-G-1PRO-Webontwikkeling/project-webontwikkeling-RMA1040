8f079bffbf0e20eab99fc160d9ba79bd80649574
Raymond Maetha wa-Meida <raymond.maethawa-meida@student.ap.be>
2024-03-11 21:01:51 +0100

    deleted weapon.json and inserted info in only one json characters.json

--- index.ts
+++ index.ts
@@ -1,67 +1,51 @@
 import * as readline from 'readline-sync';
-import characters from './json/characters.json';
-import weapons from './json/weapons.json';
+import { Character, Weapon } from './interfaces';
 
-interface Character {
-    ID: string;
-    Names: string;
-    Description: string;
-    Age: number;
-    Birthday: string;
-    isAlive: boolean;
-    imageUrl: string;
-    abilities: string[];
-    Elements: string;
-    Weapons: Weapon;
-}
-
-interface Weapon {
-    weapon_name: string;
-    weapon_id: string;
-    weapon_power: number;
-    Origin: string;
-    description: string;
-    downside: string;
-}
-
-console.log(`\x1b[36mWelcome to the Elementex!\x1b[0m\n`);
-while (true) {
-    console.log(`\x1b[32m1. View all data`);
-    console.log(`2. Filter by ID (example: CHAR-0003)\x1b[0m`);
-    console.log(`\x1b[31m3. Exit\x1b[0m\n`);
-
-    let choice = readline.questionInt("Please enter your choice: ");
-
-    if (choice === 1)
-    {
-        characters.forEach(character => {
-            console.log(`\x1b[35m- ${character.Names}\x1b[0m \x1b[96m-(${character.ID}\x1b[0m)\n  \x1b[96m- Description:\x1b[0m ${character.Description}\n  \x1b[96m- Age:\x1b[0m ${character.Age}\n  \x1b[96m- Alive:\x1b[0m ${character.isAlive}\n  \x1b[96m- Birthdate:\x1b[0m ${character.Birthday}\n  \x1b[96m- Image:\x1b[0m ${character.imageUrl}\n  \x1b[96m- Abilities:\x1b[0m ${character.abilities.join(", ")}\n  \x1b[96m- Element:\x1b[0m ${character.Elements}\n  \x1b[33m- Weapon:\x1b[0m ${character.Weapons.weapon_name}\n    \x1b[33m- WeaponID:\x1b[0m ${character.Weapons.weapon_id}\n    \x1b[33m- Weapon Power:\x1b[0m ${character.Weapons.weapon_power}\n    \x1b[33m- Origin:\x1b[0m ${character.Weapons.Origin}\n    \x1b[33m- Description:\x1b[0m ${character.Weapons.description}\n    \x1b[31m- Downside:\x1b[0m ${character.Weapons.downside}\n`);
-        });
-        console.log();
+async function fetchCharacters(): Promise<Character[]> {
+    const response = await fetch('https://raw.githubusercontent.com/AP-G-1PRO-Webontwikkeling/project-webontwikkeling-RMA1040/main/json/characters.json');
+    if (!response.ok) {
+        throw new Error('Network response was not ok.');
     }
-    else if (choice === 2)
-    {
-        const choiceID = readline.question("Please enter the ID you want to filter by: ");
-        const filteredCharacter = characters.find(character => character.ID.toUpperCase() === choiceID.toUpperCase());
-        console.log();
+    const characters: Character[] = await response.json();
+    return characters;
+}
 
-        if (filteredCharacter)
-        {
-            console.log(`\x1b[35m- ${filteredCharacter.Names}\x1b[0m \x1b[96m-(${filteredCharacter.ID}\x1b[0m)\n  \x1b[96m- Description:\x1b[0m ${filteredCharacter.Description}\n  \x1b[96m- Age:\x1b[0m ${filteredCharacter.Age}\n  \x1b[96m- Alive:\x1b[0m ${filteredCharacter.isAlive}\n  \x1b[96m- Birthdate:\x1b[0m ${filteredCharacter.Birthday}\n  \x1b[96m- Image:\x1b[0m ${filteredCharacter.imageUrl}\n  \x1b[96m- Abilities:\x1b[0m ${filteredCharacter.abilities.join(", ")}\n  \x1b[96m- Element:\x1b[0m ${filteredCharacter.Elements}\n  \x1b[33m- Weapon:\x1b[0m ${filteredCharacter.Weapons.weapon_name}\n    \x1b[33m- WeaponID:\x1b[0m ${filteredCharacter.Weapons.weapon_id}\n    \x1b[33m- Weapon Power:\x1b[0m ${filteredCharacter.Weapons.weapon_power}\n    \x1b[33m- Origin:\x1b[0m ${filteredCharacter.Weapons.Origin}\n    \x1b[33m- Description:\x1b[0m ${filteredCharacter.Weapons.description}\n    \x1b[31m- Downside:\x1b[0m ${filteredCharacter.Weapons.downside}`);
-            console.log()
-        } 
-        else
-        {
-            console.log(`Character with ID ${choiceID} not found.`);
+async function runApplication() {
+    console.log(`\x1b[36mWelcome to the Elementex!\x1b[0m\n`);
+    
+    try {
+        const characters = await fetchCharacters(); // Correctly awaiting the promise here
+
+        while (true) {
+            console.log(`\x1b[32m1. View all data`);
+            console.log(`2. Filter by ID (example: CHAR-0003)\x1b[0m`);
+            console.log(`\x1b[31m3. Exit\x1b[0m\n`);
+
+            let choice = readline.questionInt("Please enter your choice: ");
+
+            if (choice === 1) {
+                characters.forEach((character: Character) => {
+                    // Your logic to display each character's information
+                });
+                console.log();
+            } else if (choice === 2) {
+                const choiceID = readline.question("Please enter the ID you want to filter by: ");
+                const filteredCharacter = characters.find((character: Character) => character.ID.toUpperCase() === choiceID.toUpperCase());
+                if (filteredCharacter) {
+                    // Your logic to display the filtered character's information
+                } else {
+                    console.log(`Character with ID ${choiceID} not found.`);
+                }
+            } else if (choice === 3) {
+                console.log("Exiting...");
+                break;
+            } else {
+                console.log(`\x1b[31mSorry, ${choice} is not a valid choice. Try again.\x1b[0m\n`);
+            }
         }
-    } 
-    else if (choice === 3)
-    {
-        console.log("Exiting...");
-        break;
-    } 
-    else 
-    {
-        console.log(`\x1b[31mSorry, ${choice} is not a valid choice. Try again.\x1b[0m\n`);
+    } catch (error) {
+        console.error("Failed to fetch characters:", error);
     }
-};
+}
+
+runApplication();
